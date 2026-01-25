7a28a84f965d09e1c0f4669050040ae331ca57ed
Alberto Prada <albertojprada10@gmail.com>
2026-01-25 15:24:35 +0000

    change identation

--- app/sellers/[id]/page.tsx
+++ app/sellers/[id]/page.tsx
@@ -1,7 +1,7 @@
 export default function Sellers() {
-    return (
-        <div>
-            <h1>Sellers Page</h1>
-        </div>
-    )
+	return (
+		<div>
+			<h1>Sellers Page</h1>
+		</div>
+	)
 }
