891219db59b4fc7ca58aefb92048941e7e9540d0
neo06060 <saulcgaos@gmail.com>
2026-01-27 21:44:56 +0100

    commit to branch, searchbar added

--- app/ui/home/header.tsx
+++ app/ui/home/header.tsx
@@ -1,28 +1,31 @@
 import { ShoppingCartIcon, UserIcon } from "@heroicons/react/24/outline";
-// import Search from "../search";
 
 export default function Header() {
   return (
     <header className="flex flex-col gap-3 px-4 py-3 bg-background md:flex-row md:items-center md:gap-4">
 
-      {/* Row 1: Logo + search bar + icons */}
-      <div className="flex items-center justify-between w-full mx-4">
-        
+      <div className="flex items-center w-full mx-4 gap-6">
+
         {/* Logo */}
-        <div className="font-bold text-2xl text-text">
+        <div className="font-bold text-2xl text-text whitespace-nowrap">
           HCF HV
         </div>
 
-        {/* Search Bar */}
-        {/* <div className="flex-1 mx-4">
-          <Search placeholder="Search products..." />
-        </div> */}
+        {/* Search bar */}
+        <div className="flex-1">
+          <input
+            type="text"
+            placeholder="..."
+            className="w-full px-4 py-2 rounded-md text-sm border focus:outline-none text-text"
+          />
+        </div>
 
         {/* Icons */}
-        <div className="flex gap-4 mx-4">
+        <div className="flex gap-4 whitespace-nowrap">
           <ShoppingCartIcon className="h-6 w-6 cursor-pointer text-text" />
           <UserIcon className="h-6 w-6 cursor-pointer text-text" />
         </div>
+
       </div>
 
     </header>
